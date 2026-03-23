7358b08369b0e4eb3ae419dfc99e850ebe3e5431
natancasero333 <natancasero@gmail.com>
2026-03-23 15:46:02 -0300

    Update leitura.py

--- src/leitura.py
+++ src/leitura.py
@@ -8,6 +8,13 @@ def carregar_experimental(caminho_arquivo):
         names=["two_theta", "intensidade"]
     )
 
+    # forçar conversão para número
+    dados["two_theta"] = pd.to_numeric(dados["two_theta"], errors="coerce")
+    dados["intensidade"] = pd.to_numeric(dados["intensidade"], errors="coerce")
+
+    # remove qualquer linha que não seja número válido
+    dados = dados.dropna()
+
     dois_theta = dados["two_theta"].values
     intensidade = dados["intensidade"].values
 
