cf64b0f896f69473d1a45d4fcbb1d0b7b0452369
natancasero333 <natancasero@gmail.com>
2026-03-23 15:44:06 -0300

    Update leitura.py

--- src/leitura.py
+++ src/leitura.py
@@ -1,30 +1,10 @@
 import pandas as pd
 
 def carregar_experimental(caminho_arquivo):
-    """
-    Carrega um arquivo experimental de difração de raios X (.txt).
-
-    O arquivo deve conter duas colunas:
-    - two_theta (2θ em graus)
-    - intensidade
-
-    Parâmetros
-    ----------
-    caminho_arquivo : str
-        Caminho para o arquivo experimental.
-
-    Retorna
-    -------
-    dois_theta : numpy.ndarray
-        Valores de 2θ (graus).
-    intensidade : numpy.ndarray
-        Intensidades correspondentes.
-    """
-
     dados = pd.read_csv(
         caminho_arquivo,
-        sep="\t",
-        skiprows=2,
+        delim_whitespace=True,
+        skiprows=12,
         names=["two_theta", "intensidade"]
     )
 
